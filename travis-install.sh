40fe391b3fc73de23101d7d486082074f31c7929
Fergal Hainey <fergal.hainey@skyscanner.net>
2017-07-07 16:38:42 +0100

    Fix Travis CI install for no .bash_profile cache

--- travis-install.sh
+++ travis-install.sh
@@ -3,11 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 if [ ! -d "$HOME/.pyenv" ]; then
+  installpythonversions=1
   curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-  echo 'export PATH="$HOME/.pyenv/bin:$PATH"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"' >> "$HOME/.bash_profile"
-  . "$HOME/.bash_profile"
+fi
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"' >> "$HOME/.bash_profile"
+. "$HOME/.bash_profile"
+
+if [ "$installpythonversions" = "1" ]; then
   pyenv install 2.7.13
   pyenv install 3.0.1
   pyenv install 3.1.5
@@ -16,11 +21,8 @@ if [ ! -d "$HOME/.pyenv" ]; then
   pyenv install 3.4.6
   pyenv install 3.5.3
   pyenv install 3.6.1
-else
-  ls "$HOME/.pyenv"
 fi
 
-. "$HOME/.bash_profile"
 pyenv global 3.6.1 2.7.13 3.0.1 3.1.5 3.2.6 3.3.6 3.4.3 3.5.2
 # Have to pin coverage to the same version used by tox, otherwise the
 # file format changes.
